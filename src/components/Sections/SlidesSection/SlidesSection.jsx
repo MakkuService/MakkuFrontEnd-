da01a4d9a93531394e16c460affffd6ef2f18092
Bogdan <b@bmazur.ru>
2022-11-30 21:22:44 +0300

    fix: main-section

--- src/components/Sections/SlidesSection/SlidesSection.jsx
+++ src/components/Sections/SlidesSection/SlidesSection.jsx
@@ -4,7 +4,25 @@ export default function SlidesSection() {
   return (
     <section className="section section_slides">
       <h2 className="title title_slides">Как помочь?</h2>
+      <ul className="grid">
 
+        <li className="grid__item">
+          <p>
+            <span>Взять питомца</span>
+            <span>домой из приюта</span>
+          </p>
+          <li>img</li>
+        </li>
+
+        <li className="grid__item">
+          <p>
+            <span>Сделать доброе</span>
+            <span>пожертвование</span>
+          </p>
+          <li>img</li>
+        </li>
+
+      </ul>
     </section>
   );
 }
