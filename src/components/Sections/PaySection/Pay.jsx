cbed70e72a5762fbeaf56d3d7344a8565a8d8261
Bogdan <b@bmazur.ru>
2022-12-06 20:57:15 +0300

    fix: pay-form

--- src/components/Sections/PaySection/Pay.jsx
+++ src/components/Sections/PaySection/Pay.jsx
@@ -8,15 +8,28 @@ export default function Pay() {
   const handlerButton = () => {
     console.log('submit');
   };
+  const handlerTest = () => {
+    console.log('test');
+  };
 
   return (
     <section className="section section_pay">
       <form className="form form_pay">
         <Tabs />
-        <div>
+        <div className="description">
           Важна любая помощь, но именно благодаря ежемесячным пожертвованиям мы можем непрерывно помогать животным
         </div>
         <h3 className="title_pay-form">РАЗМЕР ПОЖЕРТВОВАНИЯ</h3>
+        <div className="grid">
+          <Button style="button_outline button_150" handler={handlerTest} text="100 P" />
+          <Button style="button_outline button_150" handler={handlerTest} text="200 P" />
+          <Button style="button_outline button_150" handler={handlerTest} text="500 P" />
+          <Button style="button_outline button_150" handler={handlerTest} text="1000 P" />
+        </div>
+        <div className="grid grid_20">
+          <Button style="button_outline button_325" handler={handlerTest} text="5000 P" />
+          <Button style="button_outline button_325" handler={handlerTest} text="Другая сумма" />
+        </div>
         <h3 className="title_pay-form">ВАШИ ДАННЫЕ</h3>
         <InputField />
         <InputField />
